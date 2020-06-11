0c4d7948172ad6fad021222a55d4d0d6075d9e88
Aurii <49456123+seenks1@users.noreply.github.com>
2020-06-12 01:54:20 +0700

    Create botisdead.js

---
+++ commands/botisdead.js
@@ -0,0 +1,9 @@
+module.exports = {
+	run: function (client, message, args) {
+		message.channel.send("If the bot is offline, contact 1gnite#1130 to find out when the bot comes back up again.");
+	},
+
+	help: {
+		name: "botisdead"
+	}
+};
