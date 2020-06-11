d408c5eddd14d06f300801b399bf43c078b18c69
Aurii <49456123+seenks1@users.noreply.github.com>
2020-06-12 01:54:54 +0700

    Create cmds.js

---
+++ commands/cmds.js
@@ -0,0 +1,10 @@
+module.exports = {
+  run: function (client, message, args) {
+    let commandFile = require("./help.js");
+    commandFile.run(client, message, args);
+  },
+
+  help: {
+    name: "cmds"
+  }
+};
